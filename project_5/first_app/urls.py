916bde2e03fde361028448796da4ab22ff10691b
talatmcc <talatmcc@outlook.com>
2024-08-29 23:53:25 +0800

    second practice project donme

--- project_5/first_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='homepage'),
-    path('first_app/about/', views.about, name='aboutpage'),
-    path('form/', views.form, name='formpage'),
-]
